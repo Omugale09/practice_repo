2a10f7084c53d81ed4ced386589b8e6d9bd2838b
OmUgale09 <omu1042@gmail.com>
2026-06-05 18:30:46 +0530

    initial commit

--- bhushan.py
+++ bhushan.py
@@ -1,6 +1,6 @@
 print("Hello, World!")
-<<<<<<< HEAD
+
 print ("Welcome to Python programming.")
-=======
-print("om ugale")
->>>>>>> ebc776b01c320e9d38fbceb51d51115a5d901407
+
+print("om ugale ")
+
